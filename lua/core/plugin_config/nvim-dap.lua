24b2a45cc1dae55d6bb13f0dee8c23a4f95d65b1
pytholic <rajahaseeb147@gmail.com>
2023-05-11 09:56:29 +0900

    c++ dap supp added

--- lua/core/plugin_config/nvim-dap.lua
+++ lua/core/plugin_config/nvim-dap.lua
@@ -2,6 +2,7 @@ vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", n
 vim.fn.sign_define("DapBreakpointRejected", { text = "🟦", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapStopped", { text = "⭐️", texthl = "", linehl = "", numhl = "" })
 
+-- Python --
 require("dap").configurations.python = {
 	{
 		type = "python",
@@ -14,3 +15,44 @@ require("dap").configurations.python = {
 	},
 }
 require("dap-python").setup("/opt/homebrew/bin/python3") -- Replace with your Python interpreter path
+
+-- C++ --
+local dap = require("dap")
+dap.adapters.lldb = {
+	type = "executable",
+	command = "/opt/homebrew/opt/llvm/bin/lldb-vscode", -- adjust as needed, must be absolute path
+	name = "lldb",
+}
+
+local dap = require("dap")
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+		target = "arm64",
+		-- 💀
+		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+		--
+		--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+		--
+		-- Otherwise you might get the following error:
+		--
+		--    Error on launch: Failed to attach to the target process
+		--
+		-- But you should be aware of the implications:
+		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+		-- runInTerminal = false,
+	},
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
